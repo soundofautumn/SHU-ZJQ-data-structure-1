c3726f1610b2d5c9f39f6d2fcdeb029015724c24
soundofautumn <qjm1412@163.com>
2023-11-29 08:38:23 +0800

    顺序表的验证完成

---
+++ lab1/SeqList.h
@@ -0,0 +1,216 @@
+//
+// Created by 邱姜铭 on 2023/11/28.
+//
+
+#ifndef DATA_STRUCTURE_SEQLIST_H
+#define DATA_STRUCTURE_SEQLIST_H
+
+#include <iostream>
+#include <cassert>
+
+#define DEFAULT_SIZE 100
+
+enum Status {
+    SUCCESS, OVER_FLOW, RANGE_ERROR, NOT_PRESENT, ENTRY_FOUND
+};
+
+template<class ElemType>
+class SeqList {
+protected:
+    int length;
+    int maxLength;
+    ElemType *elems;
+public:
+    explicit SeqList(int size = DEFAULT_SIZE);
+
+    SeqList(ElemType v[], int n, int size = DEFAULT_SIZE);
+
+    SeqList(const SeqList<ElemType> &sa);
+
+    virtual ~SeqList();
+
+    int GetLength() const;
+
+    bool IsEmpty() const;
+
+    void Clear();
+
+    void Traverse(void (*Visit)(const ElemType &)) const;
+
+    int LocateElem(const ElemType &e) const;
+
+    Status GetElem(int i, ElemType &e) const;
+
+    Status SetElem(int i, const ElemType &e);
+
+    Status DeleteElem(int i, ElemType &e);
+
+    Status InsertElem(int i, const ElemType &e);
+
+    Status InsertElem(const ElemType &e);
+
+    Status DelSameItem(const ElemType &e);
+
+    Status Reverse();
+
+    SeqList<ElemType> &operator=(const SeqList<ElemType> &sa);
+};
+
+template<class ElemType>
+SeqList<ElemType>::SeqList(int size) {
+    elems = new ElemType[size];
+    assert(elems);
+    maxLength = size;
+    length = 0;
+}
+
+template<class ElemType>
+SeqList<ElemType>::SeqList(ElemType v[], int n, int size) {
+    elems = new ElemType[size];
+    assert(elems);
+    maxLength = size;
+    length = n;
+    for (int i = 0; i < length; i++)
+        elems[i] = v[i];
+}
+
+template<class ElemType>
+SeqList<ElemType>::~SeqList() {
+    delete[]elems;
+}
+
+template<class ElemType>
+int SeqList<ElemType>::GetLength() const {
+    return length;
+}
+
+template<class ElemType>
+SeqList<ElemType>::SeqList(const SeqList<ElemType> &sa) {
+    elems = new ElemType[sa.maxLength];
+    assert(elems);
+    maxLength = sa.maxLength;
+    length = sa.length;
+    for (int i = 0; i < length; i++)
+        elems[i] = sa.elems[i];
+}
+
+template<class ElemType>
+SeqList<ElemType> &SeqList<ElemType>::operator=(const SeqList<ElemType> &sa) {
+    if (&sa != this) {
+        delete[]elems;
+        elems = new ElemType[sa.maxLength];
+        assert(elems);
+        maxLength = sa.maxLength;
+        length = sa.length;
+        for (int i = 0; i < length; i++)
+            elems[i] = sa.elems[i];
+    }
+    return *this;
+}
+
+template<class ElemType>
+void SeqList<ElemType>::Clear() {
+    length = 0;
+}
+
+template<class ElemType>
+void SeqList<ElemType>::Traverse(void (*visit)(const ElemType &)) const {
+    for (int i = 1; i <= length; i++)
+        (*visit)(elems[i - 1]);
+}
+
+template<class ElemType>
+int SeqList<ElemType>::LocateElem(const ElemType &e) const {
+    int i = 0;
+    while (i < length && elems[i] != e) i++;
+    return i < length ? i + 1 : 0;
+}
+
+template<class ElemType>
+Status SeqList<ElemType>::GetElem(int i, ElemType &e) const {
+    if (i < 1 || i > length) return NOT_PRESENT;
+    else {
+        e = elems[i - 1];
+        return ENTRY_FOUND;
+    }
+}
+
+template<class ElemType>
+Status SeqList<ElemType>::SetElem(int i, const ElemType &e) {
+    if (i < 1 || i > length) return RANGE_ERROR;
+    else {
+        elems[i - 1] = e;
+        return SUCCESS;
+    }
+}
+
+template<class ElemType>
+Status SeqList<ElemType>::DeleteElem(int i, ElemType &e) {
+    if (i < 1 || i > length)
+        return RANGE_ERROR;
+    else {
+        e = elems[i - 1];
+        for (int j = i; j < length; j++)
+            elems[j - 1] = elems[j];
+        length--;
+        return SUCCESS;
+    }
+}
+
+template<class ElemType>
+Status SeqList<ElemType>::InsertElem(int i, const ElemType &e) {
+    if (length == maxLength) return OVER_FLOW;
+    else if (i < 1 || i > length + 1) return RANGE_ERROR;
+    else {
+        for (int j = length; j >= i; j--)
+            elems[j] = elems[j - 1];
+        elems[i - 1] = e;
+        length++;
+        return SUCCESS;
+    }
+}
+
+template<class ElemType>
+Status SeqList<ElemType>::InsertElem(const ElemType &e) {
+    if (length == maxLength)
+        return OVER_FLOW;
+    else {
+        elems[length] = e;
+        length++;
+        return SUCCESS;
+    }
+}
+
+template<class ElemType>
+bool SeqList<ElemType>::IsEmpty() const {
+    return length == 0;
+}
+
+template<class ElemType>
+Status SeqList<ElemType>::DelSameItem(const ElemType &e) {
+    int i = 0, j = 0;
+    while (j < length) {
+        if (elems[j] != e) {
+            elems[i] = elems[j];
+            i++;
+        }
+        j++;
+    }
+    length = i;
+    return SUCCESS;
+}
+
+template<class ElemType>
+Status SeqList<ElemType>::Reverse() {
+    int i = 0, j = length - 1;
+    while (i < j) {
+        ElemType temp = elems[i];
+        elems[i] = elems[j];
+        elems[j] = temp;
+        i++;
+        j--;
+    }
+    return SUCCESS;
+}
+
+#endif //DATA_STRUCTURE_SEQLIST_H
---
+++ lab1/main.cpp
@@ -0,0 +1,51 @@
+//
+// Created by 邱姜铭 on 2023/11/28.
+//
+#include <iostream>
+#include "SeqList.h"
+#include "Point2D.h"
+
+using namespace std;
+
+int main() {
+    SeqList<Point2D> list;
+    auto visit = [](const Point2D &point2D) {
+        cout << point2D << endl;
+    };
+    cout << "The list is empty:"
+         << list.IsEmpty() << endl;
+    list.InsertElem(Point2D(1, 2));
+    list.InsertElem(Point2D(2, 3));
+    list.InsertElem(Point2D(2, 3));
+    list.InsertElem(Point2D(3, 3));
+    cout << "The length of the list is:"
+         << list.GetLength() << endl;
+    Point2D point2D;
+    cout << "The first element is:"
+         << list.GetElem(1, point2D) << endl;
+    cout << "The list is:" << endl;
+    list.Traverse(visit);
+    cout << "Reverse list test" << endl;
+    list.Reverse();
+    cout << "The list is:" << endl;
+    list.Traverse(visit);
+    point2D = Point2D(2, 3);
+    cout << "The location of the point is:"
+         << list.LocateElem(point2D) << endl;
+    cout << "Delete same item test" << endl;
+    list.DelSameItem(point2D);
+    cout << "The list is:" << endl;
+    list.Traverse(visit);
+    cout << "The length of the list is:"
+         << list.GetLength() << endl;
+    list.Clear();
+    cout << "The list is empty:"
+         << list.IsEmpty() << endl;
+    cout << "The length of the list is:"
+         << list.GetLength() << endl;
+    list.InsertElem(Point2D(1, 2));
+    cout << "The list is empty:"
+         << list.IsEmpty() << endl;
+    cout << "The length of the list is:"
+         << list.GetLength() << endl;
+}
