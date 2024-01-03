ff614869c0266c6d27b693a14033b3b66036f680
soundofautumn <qjm1412@163.com>
2024-01-03 08:43:49 +0800

    lab3-1

---
+++ lab3-1/KMP.h
@@ -0,0 +1,73 @@
+#ifndef __KMP_H__
+#define __KMP_H__
+
+#include "String.h"                // 串类
+
+// KMP匹配算法
+void GetFailure(const String &pat, int f[])
+// 操作结果: 求模式串pat的失效函数值
+{
+    f[0] = -1;                             // 初始f[0]的值为-1
+    int j = 0, k = -1;
+    while (j < pat.GetLength() - 1)
+        if (k == -1 || pat[k] == pat[j]) {
+            // 改进后的失效函数
+            if (pat[k] != pat[j]) {
+                f[++j] = ++k;
+            } else {
+                f[++j] = f[++k];
+            }
+        } else                             // pat[k]与pat[j]不匹配
+            k = f[k];                     // 寻求新的匹配字符
+}
+
+
+int KMP_find(const String &ob, const String &pat, int p = 0)
+// 操作结果: 查找模式串pat第一次在目标串ob中从第p个字符开始出现的位置
+{
+    int *f = new int[pat.GetLength()];      // 为失效函数值数组f分配空间
+    GetFailure(pat, f);                     // 求模式串pat的f数组的元素值
+    int i = p, j = 0;
+    while (i < ob.GetLength() && j < pat.GetLength() && pat.GetLength() - j <= ob.GetLength() - i)
+        if (j == -1 || pat[j] == ob[i]) {
+            i++;
+            j++;                         // 模式串pat与目标串ob的当前位置向后移
+        } else                                 // pat[j]与ob[i]不匹配
+            j = f[j];                         // 寻找新的模式串pat的匹配字符位置
+    delete[]f;                             // 释f所占用的存储空间
+    if (j < pat.GetLength())
+        return -1;                         // 匹配失败
+    else
+        return i - j;                     // 匹配成功
+}
+
+// KMP匹配算法
+int KMP_find_all(const String &ob, const String &pat, int *&pos) {
+    int *f = new int[pat.GetLength()];      // 为失效函数值数组f分配空间
+    GetFailure(pat, f);                     // 求模式串pat的f数组的元素值
+    int i = 0, j = 0, count = 0;
+    pos = new int[ob.GetLength()];          // 为pos分配空间
+    while (true) {
+        while (i < ob.GetLength() && j < pat.GetLength() && pat.GetLength() - j <= ob.GetLength() - i) {
+            if (j == -1 || pat[j] == ob[i]) {
+                i++;
+                j++;                         // 模式串pat与目标串ob的当前位置向后移
+            } else {                       // pat[j]与ob[i]不匹配
+                j = f[j];                         // 寻找新的模式串pat的匹配字符位置
+            }
+        }
+        if (j < pat.GetLength()) {
+            break;                        // 匹配失败
+        } else {
+            pos[count++] = i - j;             // 匹配成功
+            i = i - j + 1;                     // 从下一个位置开始匹配
+            j = 0;
+        }
+
+    }
+    delete[]f;                             // 释放所占用的存储空间
+    return count;                         // 返回匹配次数
+}
+
+#endif
+
---
+++ lab3-1/TestKMP.cpp
@@ -0,0 +1,67 @@
+#include "KMP.h"              // KMP算法
+
+int main() {    // 测式KMP字符串模式匹配算法
+    String ob, pat;         // 主串和模式串
+
+    cout<<"请依次输入主串和模式串："<<endl;
+    // 读入主串和模式串 各串以回车结束
+    std::string str;
+    getline(cin, str);
+    ob = str.c_str();
+    getline(cin, str);
+    pat = str.c_str();
+
+    cout << "主串为：";
+    Write(ob);
+    cout << "模式串为：";
+    Write(pat);
+
+    cout << endl << "从主串开始位置（第0个字符）查找模式串：";
+    cout << KMP_find(ob, pat) << endl;
+
+    cout << endl << "从主串第3个字符开始查找模式串：";
+    cout << KMP_find(ob, pat, 3) << endl;
+
+    pat = "string";
+    cout << endl << "改模式串为string，从主串开始位置查找模式串：";
+    cout << KMP_find(ob, pat) << endl;
+
+    pat = "this";
+    cout << endl << "改模式串为this，从主串开始位置查找模式串：";
+    cout << KMP_find(ob, pat) << endl;
+
+    cout << endl << "改模式串为this，从主串第1个字符开始查找模式串：";
+    cout << KMP_find(ob, pat, 1) << endl;
+
+    pat = "that";
+    cout << endl << "改模式串为that，从主串开始位置查找模式串：";
+    cout << KMP_find(ob, pat) << endl;
+
+    pat = "this is a string!";
+    cout << endl << "改模式串为this is a string!，从主串开始位置查找模式串：";
+    cout << KMP_find(ob, pat) << endl;
+
+    pat = ob;
+    cout << endl << "改模式串为this is a string，从主串开始位置查找模式串：";
+    cout << KMP_find(ob, pat) << endl;
+
+    // 测试KMP_find_all
+    int *pos;
+    ob = "this is a string";
+    pat = "is";
+    cout << endl << "改模式串为is，从主串开始位置查找模式串：";
+    cout << KMP_find_all(ob, pat, pos) << endl;
+    cout << "模式串is在主串中出现的位置为：";
+    for (int i = 0; i < KMP_find_all(ob, pat, pos); i++)
+        cout << pos[i] << " ";
+    cout << endl;
+
+#ifdef _WIN32
+    system("PAUSE");        // 调用库函数system()
+#endif
+#if defined( __APPLE__) || defined(__linux__)
+    cout << "Please press any key to continue..." << endl;
+    system("read");
+#endif
+    return 0;               // 返回值0, 返回操作系统
+}
